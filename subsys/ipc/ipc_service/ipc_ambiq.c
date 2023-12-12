15713ca4a3ca7bbb24c6fa403ba328c0468584f5
Bryan Zhu <bzhu@ambiq.com>
2023-12-12 08:44:53 +0800

    ipc ambiq is not not able to work due to receiving relies on some VQ parameters which is instance private data

--- subsys/ipc/ipc_service/ipc_ambiq.c
+++ subsys/ipc/ipc_service/ipc_ambiq.c
@@ -13,14 +13,20 @@
 
 LOG_MODULE_DECLARE(ipc_service, CONFIG_IPC_SERVICE_LOG_LEVEL);
 
+#define STATE_READY	(0)
+#define STATE_BUSY	(1)
+#define STATE_INITED	(2)
+
 #define WQ_STACK_SIZE         (512)
 #define IPC_AMBIQ_WQ_PRIORITY (K_PRIO_COOP(1))
 K_THREAD_STACK_DEFINE(ipc_ambiq_stack, WQ_STACK_SIZE);
 
 struct ipc_ambiq_data {
 	/* IPC work */
-	struct k_work ipc_ambiq_work;
+	struct k_work ipc_tx_work;
+	struct k_work ipc_rx_work;
 	struct k_work_q ipc_ambiq_wq;
+
 };
 
 static struct ipc_ambiq_data ipc_data;
@@ -49,6 +55,74 @@ int ipc_ambiq_deregister_endpoint(struct ipc_ept *ept)
 
 int ipc_ambiq_send(struct ipc_ept *ept, const void *data, size_t len)
 {
+	// struct ipc_ambiq_data *data = CONTAINER_OF(cb, struct ipc_ambiq_data, irq_gpio_cb);
+	// err = k_work_submit_to_queue(&ipc_data.ipc_ambiq_wq, &(ipc_data.ipc_tx_work));
+	// if (err < 0) {
+	// 	LOG_ERR("IPC work submit failed=%d\n", err);
+	// }
 	return ipc_service_send(ept, data, len);
 }
 
+static void ipc_tx_work_process(struct k_work *item)
+{
+	// struct ipc_ambiq_data *data = CONTAINER_OF(item, struct ipc_ambiq_data, ipc_tx_work);
+	// ipc_service_send(ept, data, len);
+}
+
+static void ipc_rx_work_process(struct k_work *item)
+{
+	struct ipc_ambiq_data *data = CONTAINER_OF(item, struct ipc_ambiq_data, ipc_rx_work);
+	// ipc_service_send(ept, data, len);
+}
+
+static void mbox_callback(const struct device *instance, uint32_t channel,
+			  void *user_data, struct mbox_msg *msg_data)
+{
+	struct ipc_ambiq_data *data = user_data;
+
+	k_work_submit_to_queue(&ipc_data.ipc_ambiq_wq, &ipc_data.ipc_rx_work);
+}
+
+int ipc_ambiq_config_instance(const struct device *instance, struct mbox_channel *mbox_rx)
+{
+	int err;
+	if(!device_is_ready(instance))
+	{
+		LOG_ERR("Instance is not ready");
+		return -ENODEV;
+	}
+
+	err = mbox_register_callback(mbox_rx, mbox_callback, &ipc_data);
+	if (err != 0) {
+		LOG_ERR("Overload mbox isr failed:%d", err);
+		return err;
+	}
+	/**
+	 * Initialize work item for handling IPC serialized TX/RX
+	 */
+	if (!atomic_cas(&data->state, STATE_READY, STATE_BUSY)) {
+		return -EALREADY;
+	}
+	err = k_work_queue_init(&ipc_data.ipc_ambiq_wq);
+	if (err != 0) {
+		LOG_ERR("Init ipc WQ failed:%d", err);
+		return err;
+	}
+	err = k_work_queue_start(&ipc_data.ipc_ambiq_wq, ipc_ambiq_stack, WQ_STACK_SIZE,
+			   IPC_AMBIQ_WQ_PRIORITY, NULL);
+	if (err != 0) {
+		LOG_ERR("Start ipc WQ failed:%d", err);
+		return err;
+	}
+	err = k_work_init(&ipc_data.ipc_tx_work, ipc_tx_work_process);
+	if (err != 0) {
+		LOG_ERR("Init ipc work item failed:%d", err);
+		return err;
+	}
+	err = k_work_init(&ipc_data.ipc_rx_work, ipc_rx_work_process);
+	if (err != 0) {
+		LOG_ERR("Init ipc work item failed:%d", err);
+		return err;
+	}
+	atomic_set(&data->state, STATE_INITED);
+}
