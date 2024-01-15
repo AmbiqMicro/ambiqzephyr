10016a0bc2a9f7cd29ee7087016e2172af8b7c44
zwang <zwang@ambiq.com>
2024-01-15 15:31:13 +0800

    optimized the stimer driver

--- drivers/timer/ambiq_stimer.c
+++ drivers/timer/ambiq_stimer.c
@@ -24,11 +24,10 @@
 
 #define COUNTER_MAX UINT32_MAX
 
-#define CYC_PER_TICK (sys_clock_hw_cycles_per_sec()	\
-		      / CONFIG_SYS_CLOCK_TICKS_PER_SEC)
-#define MAX_TICKS ((k_ticks_t)(COUNTER_MAX / CYC_PER_TICK) - 1)
-#define MAX_CYCLES (MAX_TICKS * CYC_PER_TICK)
-#define MIN_DELAY 1
+#define CYC_PER_TICK (sys_clock_hw_cycles_per_sec() / CONFIG_SYS_CLOCK_TICKS_PER_SEC)
+#define MAX_TICKS    ((k_ticks_t)(COUNTER_MAX / CYC_PER_TICK) - 1)
+#define MAX_CYCLES   (MAX_TICKS * CYC_PER_TICK)
+#define MIN_DELAY    1
 
 #define TIMER_IRQ (DT_INST_IRQN(0))
 
@@ -53,19 +52,43 @@ static void stimer_isr(const void *arg)
 
 		k_spinlock_key_t key = k_spin_lock(&g_lock);
 
+		/* Read current cycle count. */
 		uint32_t now = am_hal_stimer_counter_get();
-		uint32_t dticks = (uint32_t)((now - g_last_count) / CYC_PER_TICK);
 
-		g_last_count += dticks * CYC_PER_TICK;
+		/* Get elapsed cycles, compare current count and recorded count to
+		 * cope with the hardware counter overflow condition.
+		 */
+		uint32_t elapsed_cycle = (now >= g_last_count) ? (now - g_last_count)
+							       : (now + COUNTER_MAX - g_last_count);
+
+		/* Get elapsed ticks. */
+		uint32_t dticks = elapsed_cycle / CYC_PER_TICK;
 
 		if (!IS_ENABLED(CONFIG_TICKLESS_KERNEL)) {
-			uint32_t next = g_last_count + CYC_PER_TICK;
+			/* Get the counter value to trigger the next tick interrupt. */
+			uint32_t next = now + (dticks + 1) * CYC_PER_TICK;
 
-			if ((int32_t)(next - now) < MIN_DELAY) {
-				next += CYC_PER_TICK;
+			/* Read again to prevent time skew */
+			now = am_hal_stimer_counter_get();
+
+			/* Calculate the delta value. */
+			uint32_t delta = (next >= now) ? (next - now) : (next + COUNTER_MAX - now);
+
+			/* A delta value smaller than MIN_DELAY means the stimer interrupt will be
+			 * trigger again as soon as we exits current ISR. We will prevent this
+			 * situation by increasing the dticks and delta as if the interrupt has
+			 * already been triggered.
+			 */
+			if (delta < MIN_DELAY) {
+				dticks += 1;
+				delta += CYC_PER_TICK;
 			}
-			am_hal_stimer_compare_delta_set(0, next - g_last_count);
+
+			/* Set delta. */
+			am_hal_stimer_compare_delta_set(0, delta);
 		}
+
+		g_last_count += dticks * CYC_PER_TICK;
 
 		k_spin_unlock(&g_lock, key);
 		sys_clock_announce(dticks);
@@ -80,17 +103,53 @@ void sys_clock_set_timeout(int32_t ticks, bool idle)
 		return;
 	}
 
-	if (ticks == K_TICKS_FOREVER) {
-		return;
-	}
-
-	ticks = MIN(MAX_TICKS, ticks);
-	/* If tick is 0, set delta cyc to MIN_DELAY to trigger tick isr asap */
-	uint32_t cyc = MAX(ticks * CYC_PER_TICK, MIN_DELAY);
+	/* Adjust the ticks to the range of [0, MAX_TICKS]. */
+	ticks = (ticks == K_TICKS_FOREVER) ? MAX_TICKS : ticks;
+	ticks = CLAMP(ticks, 1, (int32_t)MAX_TICKS);
 
 	k_spinlock_key_t key = k_spin_lock(&g_lock);
 
-	am_hal_stimer_compare_delta_set(0, cyc);
+	/* Get current hardware counter value.*/
+	uint32_t current = am_hal_stimer_counter_get();
+
+	/* announced: the last announced counter value.
+	 * now: current counter value. Extended to uint64_t to easy the handing of hardware counter
+	 * overflow. next: counter values where to trigger the scheduled timeout. upperBoundary: the
+	 * maximal counter value allowed to be set, if a timeout interrupt triggered after this
+	 * point, the gap between that triggered point and the last announced point will overflow
+	 * uint32_t. announced < now < next
+	 */
+	uint64_t announced = (uint64_t)g_last_count;
+	uint64_t now =
+		(g_last_count < current) ? (uint64_t)current : (uint64_t)current + COUNTER_MAX;
+	uint64_t next = now + ticks * CYC_PER_TICK;
+	uint64_t upperBoundary = announced + MAX_CYCLES;
+
+	uint32_t delta;
+
+	if (upperBoundary < now) {
+		/* This only happens in a corner case where the following
+		 * two conditions satisfied at the same time:
+		 * 1. new timeouts keep being set before the existing one fires.
+		 * 2. stimer interrupt is pended for over 2ms before it is handled.
+		 */
+		delta = MIN_DELAY;
+	} else if (upperBoundary <= next) {
+		delta = upperBoundary - now;
+	} else {
+		uint32_t gap = next - announced;
+		uint32_t gapAligned = (gap / CYC_PER_TICK) * CYC_PER_TICK;
+		uint64_t nextAligned = announced + gapAligned;
+
+		delta = nextAligned - now;
+	}
+
+	if (delta <= MIN_DELAY) {
+		/*If the delta value is smaller than MIN_DELAY, trigger a interrupt immediately*/
+		am_hal_stimer_int_set(AM_HAL_STIMER_INT_COMPAREA);
+	} else {
+		am_hal_stimer_compare_delta_set(0, delta);
+	}
 
 	k_spin_unlock(&g_lock, key);
 }
@@ -102,8 +161,12 @@ uint32_t sys_clock_elapsed(void)
 	}
 
 	k_spinlock_key_t key = k_spin_lock(&g_lock);
-	uint32_t ret = (am_hal_stimer_counter_get()
-			- g_last_count) / CYC_PER_TICK;
+
+	uint32_t now = am_hal_stimer_counter_get();
+	uint32_t elapsed =
+		(now >= g_last_count) ? (now - g_last_count) : (now + COUNTER_MAX - g_last_count);
+
+	uint32_t ret = elapsed / CYC_PER_TICK;
 
 	k_spin_unlock(&g_lock, key);
 	return ret;
@@ -122,13 +185,11 @@ static int stimer_init(void)
 	oldCfg = am_hal_stimer_config(AM_HAL_STIMER_CFG_FREEZE);
 
 #if defined(CONFIG_SOC_SERIES_APOLLO3X)
-	am_hal_stimer_config((oldCfg & ~(AM_HAL_STIMER_CFG_FREEZE | CTIMER_STCFG_CLKSEL_Msk))
-			| AM_HAL_STIMER_XTAL_32KHZ
-			| AM_HAL_STIMER_CFG_COMPARE_A_ENABLE);
+	am_hal_stimer_config((oldCfg & ~(AM_HAL_STIMER_CFG_FREEZE | CTIMER_STCFG_CLKSEL_Msk)) |
+			     AM_HAL_STIMER_XTAL_32KHZ | AM_HAL_STIMER_CFG_COMPARE_A_ENABLE);
 #else
-	am_hal_stimer_config((oldCfg & ~(AM_HAL_STIMER_CFG_FREEZE | STIMER_STCFG_CLKSEL_Msk))
-			| AM_HAL_STIMER_XTAL_32KHZ
-			| AM_HAL_STIMER_CFG_COMPARE_A_ENABLE);
+	am_hal_stimer_config((oldCfg & ~(AM_HAL_STIMER_CFG_FREEZE | STIMER_STCFG_CLKSEL_Msk)) |
+			     AM_HAL_STIMER_XTAL_32KHZ | AM_HAL_STIMER_CFG_COMPARE_A_ENABLE);
 #endif
 	g_last_count = am_hal_stimer_counter_get();
 
@@ -146,5 +207,4 @@ static int stimer_init(void)
 	return 0;
 }
 
-SYS_INIT(stimer_init, PRE_KERNEL_2,
-	 CONFIG_SYSTEM_CLOCK_INIT_PRIORITY);
+SYS_INIT(stimer_init, PRE_KERNEL_2, CONFIG_SYSTEM_CLOCK_INIT_PRIORITY);
