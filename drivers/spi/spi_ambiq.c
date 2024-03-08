f4a78c95d39d347314e803178b5fc8ce4518d644
Hao Luo <hluo@ambiq.com>
2024-03-08 17:34:23 +0800

    1. Add support for more bytes of instruction in tx mode 2. Restructured spi_test sample to simulate more scenarios

--- drivers/spi/spi_ambiq.c
+++ drivers/spi/spi_ambiq.c
@@ -203,8 +203,8 @@ static int spi_ambiq_xfer(const struct device *dev, const struct spi_config *con
 			if (ctx->rx_buf != NULL) {
 				/* More instruction bytes to send */
 				if (ctx->tx_len > 0) {
-					/* The instruction length can only be 0~5. */
-					if (ctx->tx_len > 4) {
+					/* The instruction length can only be 0~AM_HAL_IOM_MAX_OFFSETSIZE. */
+					if (ctx->tx_len > AM_HAL_IOM_MAX_OFFSETSIZE - 1) {
 						spi_context_complete(ctx, dev, 0);
 						return -ENOTSUP;
 					}
@@ -242,6 +242,23 @@ static int spi_ambiq_xfer(const struct device *dev, const struct spi_config *con
 				ret = am_hal_iom_blocking_transfer(data->IOMHandle, &trans);
 #endif
 			} else {
+                /* More instruction bytes to send */
+                /* The instruction length can only be 0~AM_HAL_IOM_MAX_OFFSETSIZE. */
+                if (ctx->tx_len > AM_HAL_IOM_MAX_OFFSETSIZE - 1) {
+                    spi_context_complete(ctx, dev, 0);
+                    return -ENOTSUP;
+                }
+
+                /* Put the remaining TX data in instruction. */
+                trans.ui32InstrLen += ctx->tx_len;
+                for (int i = 0; i < trans.ui32InstrLen - 1; i++) {
+#if defined(CONFIG_SOC_SERIES_APOLLO3X)
+                    trans.ui32Instr = (trans.ui32Instr << 8) | (*ctx->tx_buf);
+#else
+                    trans.ui64Instr = (trans.ui64Instr << 8) | (*ctx->tx_buf);
+#endif
+                    spi_context_update_tx(ctx, 1, 1);
+                }
 				/* Set TX direction to send data. */
 				trans.eDirection = AM_HAL_IOM_TX;
 				trans.bContinue = bContinue;
--- samples/boards/ambiq/spi_test/src/main.c
+++ samples/boards/ambiq/spi_test/src/main.c
@@ -20,69 +20,200 @@ static const struct device *spi_dev = DEVICE_DT_GET(TESE_SPI);
 #error "Node is disabled"
 #endif
 
-#define TEST_FREQ_HZ 8000000U	//48000000U
+#define TEST_FREQ_HZ 8000000U // 48000000U
 
-#define XOR_BYTE            0
+#define XOR_BYTE 0
 
-static struct spi_config spi_cfg_single = {
+#define DATA_SIZE 10
+
+static struct spi_config spi_cfg = {
 	.frequency = TEST_FREQ_HZ,
-	.operation = (SPI_OP_MODE_MASTER | SPI_TRANSFER_MSB | SPI_WORD_SET(8)
-		      | SPI_LINES_SINGLE),
+	.operation = (SPI_HALF_DUPLEX | SPI_OP_MODE_MASTER | SPI_TRANSFER_MSB | SPI_WORD_SET(8) |
+		      SPI_LINES_SINGLE),
 };
 
-struct spi_buf_set tx_set;
-struct spi_buf_set rx_set;
+static int ambiq_spi_rdsr(const struct device *dev, uint8_t *status)
+{
+	uint8_t rdsr = 0x05;
+	uint8_t sr[2];
+	int err;
+	const struct spi_buf tx_buf = {
+		.buf = &rdsr,
+		.len = 1,
+	};
+	const struct spi_buf_set tx = {
+		.buffers = &tx_buf,
+		.count = 1,
+	};
+	const struct spi_buf rx_buf = {
+		.buf = sr,
+		.len = sizeof(sr),
+	};
+	const struct spi_buf_set rx = {
+		.buffers = &rx_buf,
+		.count = 1,
+	};
+
+	err = spi_transceive(dev, &spi_cfg, &tx, &rx);
+	if (!err) {
+		*status = sr[1];
+	}
+
+	return err;
+}
+
+static int ambiq_spi_write(const struct device *dev, off_t offset, const void *buf, size_t len)
+{
+	uint8_t cmd[3] = {0x02, 0, 0};
+	uint8_t *paddr;
+	int err;
+	const struct spi_buf tx_bufs[2] = {
+		{
+			.buf = cmd,
+			.len = 3,
+		},
+		{
+			.buf = (void *)buf,
+			.len = len,
+		},
+	};
+	const struct spi_buf_set tx = {
+		.buffers = tx_bufs,
+		.count = ARRAY_SIZE(tx_bufs),
+	};
+	paddr = &cmd[1];
+	*paddr++ = offset >> 8;
+	*paddr = offset;
+	err = spi_transceive(dev, &spi_cfg, &tx, NULL);
+	if (err) {
+		return err;
+	}
+
+	return err;
+}
+
+static int ambiq_spi_read(const struct device *dev, off_t offset, void *buf, size_t len, bool bFd,
+			  bool bCont)
+{
+	uint8_t cmd[3] = {0x03, 0, 0};
+	uint8_t *paddr;
+	int err;
+	paddr = &cmd[1];
+	*paddr++ = offset >> 8;
+	*paddr = offset;
+
+	const struct spi_buf rx_buf = {
+		.buf = buf,
+		.len = len,
+	};
+	const struct spi_buf_set rx = {
+		.buffers = &rx_buf,
+		.count = 1,
+	};
+
+	if (bCont) {
+		spi_cfg.operation |= SPI_HOLD_ON_CS;
+	} else {
+		spi_cfg.operation &= ~SPI_HOLD_ON_CS;
+	}
+
+	if (bFd) {
+		spi_cfg.operation &= ~SPI_HALF_DUPLEX;
+		void *cmd_tx = NULL;
+		cmd_tx = k_malloc(len);
+		memset(cmd_tx, 0, len);
+		memcpy(cmd_tx, cmd, 3);
+		const struct spi_buf tx_buf = {
+			.buf = cmd_tx,
+			.len = len,
+		};
+		const struct spi_buf_set tx = {
+			.buffers = &tx_buf,
+			.count = 1,
+		};
+		err = spi_transceive(dev, &spi_cfg, &tx, &rx);
+		if (err < 0) {
+			return err;
+		}
+	} else {
+		spi_cfg.operation |= SPI_HALF_DUPLEX;
+		const struct spi_buf tx_buf = {
+			.buf = cmd,
+			.len = 3,
+		};
+		const struct spi_buf_set tx = {
+			.buffers = &tx_buf,
+			.count = 1,
+		};
+		err = spi_transceive(dev, &spi_cfg, &tx, &rx);
+		if (err < 0) {
+			return err;
+		}
+	}
+
+	return err;
+}
+
+static int ambiq_spi_read_continue(const struct device *dev, void *buf, size_t len, bool bCont)
+{
+	int err;
+	const struct spi_buf rx_buf = {
+		.buf = buf,
+		.len = len,
+	};
+	const struct spi_buf_set rx = {
+		.buffers = &rx_buf,
+		.count = 1,
+	};
+
+	if (bCont) {
+		spi_cfg.operation |= SPI_HOLD_ON_CS;
+	} else {
+		spi_cfg.operation &= ~SPI_HOLD_ON_CS;
+	}
+	err = spi_transceive(dev, &spi_cfg, NULL, &rx);
+	if (err < 0) {
+		return err;
+	}
+
+	return err;
+}
 
 int main(void)
 {
 	int ret;
+	uint8_t status = 0;
 
 	if (!device_is_ready(spi_dev)) {
 		printf("SPI 0 device not ready!\n");
 		return -1;
 	}
 
-	#define DATA_SIZE	1024
 	uint8_t buff[DATA_SIZE] = {0};
 	// Initialize Test Data
 	for (uint32_t i = 0; i < DATA_SIZE; i++) {
 		buff[i] = (i & 0xFF) ^ XOR_BYTE;
 	}
-	uint8_t rxdata[DATA_SIZE];
+	uint8_t rxdata[10][DATA_SIZE];
 
-	struct spi_buf tx_buf[1] = {
-		{.buf = buff, .len = DATA_SIZE},
-	};
-	struct spi_buf rx_buf[1] = {
-		{.buf = rxdata, .len = 0},
-	};
+	/* Read register */
+	ret = ambiq_spi_rdsr(spi_dev, &status);
 
-	tx_set.buffers = tx_buf;
-	tx_set.count = 1;
+	/* Write memory */
+	ret = ambiq_spi_write(spi_dev, 0x1FFF, buff, DATA_SIZE);
 
-	spi_cfg_single.operation |= SPI_HALF_DUPLEX;
-	// Half duplex tx
-	ret = spi_transceive(spi_dev, &spi_cfg_single, &tx_set, NULL);
+	/* Read memory */
+	ret = ambiq_spi_read(spi_dev, 0x1FFF, rxdata[0], DATA_SIZE, false, false);
 
-	rx_set.buffers = rx_buf;
-	rx_buf[0].len = DATA_SIZE;
-	rx_set.count = 1;
-	spi_cfg_single.operation &= ~SPI_HALF_DUPLEX;
-	// Full duplex
-	ret = spi_transceive(spi_dev, &spi_cfg_single, &tx_set, &rx_set);
+	/* Read memory continue */
+	ret = ambiq_spi_read(spi_dev, 0x1FFF, rxdata[0], DATA_SIZE, false, true);
+	for (uint8_t i = 1; i < 10; i++) {
+		ret = ambiq_spi_read_continue(spi_dev, rxdata[i], DATA_SIZE,
+					      (i == 9 ? false : true));
+	}
 
-	memset(buff, 0x5A, DATA_SIZE);
-	tx_buf[0].len = 1;
-	rx_buf[0].len = 0;
-	spi_cfg_single.operation |= SPI_HOLD_ON_CS | SPI_HALF_DUPLEX;
-	// Half duplex tx continue
-	ret = spi_transceive(spi_dev, &spi_cfg_single, &tx_set, &rx_set);
-
-	tx_buf[0].len = 0;
-	rx_buf[0].len = DATA_SIZE;
-	spi_cfg_single.operation &= ~SPI_HOLD_ON_CS;
-	// Half duplex rx
-	ret = spi_transceive(spi_dev, &spi_cfg_single, &tx_set, &rx_set);
+	/* Fullduplex */
+	ret = ambiq_spi_read(spi_dev, 0x1FFF, rxdata[0], DATA_SIZE, true, false);
 
 	return 0;
 }
