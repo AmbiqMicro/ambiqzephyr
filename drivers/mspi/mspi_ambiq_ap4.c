bc63c20d972cf7b1d2654941db7c2a04590e25c4
Richard Wheatley <richard.wheatley@ambiq.com>
2024-04-30 09:15:00 -0500

    drivers: mspi: change how DQS is handled
    Check for Device DQS enable.
    Changed RX CFG to MSPI Device Init.
    
    Signed-off-by: Richard Wheatley <richard.wheatley@ambiq.com>

--- drivers/mspi/mspi_ambiq_ap4.c
+++ drivers/mspi/mspi_ambiq_ap4.c
@@ -39,20 +39,6 @@ am_hal_mspi_timing_scan_t g_sApplyCfg =
 	.ui8Turnaround     = 6,
 };
 
-am_hal_mspi_rxcfg_t g_sMspiRxCfg =
-{
-	.ui8DQSturn         = 2,
-	.bRxHI              = 0,
-	.bTaForth           = 0,
-	.bHyperIO           = 0,
-	.ui8RxSmp           = 1,
-	.bRBX               = 0,
-	.bWBX               = 0,
-	.bSCLKRxHalt        = 0,
-	.bRxCapEXT          = 0,
-	.ui8Sfturn          = 0,
-};
-
 am_hal_mspi_dqs_t g_sMspiDqsCfg =
 {
 	.bDQSEnable             = false,
@@ -100,6 +86,7 @@ struct mspi_ambiq_data {
 	am_hal_mspi_config_t            hal_config_cfg;
 	am_hal_mspi_xip_config_t        hal_xip_cfg;
 	am_hal_mspi_xip_misc_t          hal_xip_misc_cfg;
+	am_hal_mspi_rxcfg_t             hal_rx_cfg;
 
 	struct mspi_dev_id              *dev_id;
 	struct k_mutex                  lock;
@@ -468,11 +455,6 @@ static int mspi_ambiq_config(const struct mspi_dt_spec *spec)
 		return -ENOTSUP;
 	}
 
-	if (config->bDQS) {
-		LOG_INST_ERR(cfg->log, "%u, only support non-DQS mode.", __LINE__);
-		return -ENOTSUP;
-	}
-
 	if (config->bReinit) {
 		ret = mspi_ambiq_deinit(spec->bus);
 		if (ret) {
@@ -548,6 +530,7 @@ static int mspi_ambiq_dev_config(const struct device *controller,
 	const struct mspi_ambiq_config *cfg = controller->config;
 	struct mspi_ambiq_data *data = controller->data;
 	am_hal_mspi_dev_config_t hal_dev_cfg = data->hal_dev_cfg;
+
 	int ret = 0;
 
 	if (data->dev_id != dev_id) {
@@ -637,15 +620,6 @@ static int mspi_ambiq_dev_config(const struct device *controller,
 				goto e_return;
 			}
 			hal_dev_cfg.eInstrCfg = dev_cfg->ui16InstrLength - 1;
-			// ret = am_hal_mspi_control(data->mspiHandle,
-			// 			  AM_HAL_MSPI_REQ_ISIZE_SET,
-			// 			  &hal_dev_cfg.eInstrCfg);
-			if (ret) {
-				LOG_INST_ERR(cfg->log, "%u, failed to configure ui16InstrLength.",
-						 __LINE__);
-				ret = -EHOSTDOWN;
-				goto e_return;
-			}
 			data->dev_cfg.ui16InstrLength = dev_cfg->ui16InstrLength;
 		}
 
@@ -657,15 +631,6 @@ static int mspi_ambiq_dev_config(const struct device *controller,
 				goto e_return;
 			}
 			hal_dev_cfg.eAddrCfg = dev_cfg->ui16AddrLength - 1;
-			// ret = am_hal_mspi_control(data->mspiHandle,
-			// 			AM_HAL_MSPI_REQ_ASIZE_SET,
-			// 			&hal_dev_cfg.eAddrCfg);
-			if (ret) {
-				LOG_INST_ERR(cfg->log, "%u, failed to configure ui16AddrLength.",
-						 __LINE__);
-				ret = -EHOSTDOWN;
-				goto e_return;
-			}
 			data->dev_cfg.ui16AddrLength = dev_cfg->ui16AddrLength;
 		}
 
@@ -682,12 +647,6 @@ static int mspi_ambiq_dev_config(const struct device *controller,
 
 		if (dev_cfg->eEndian != MSPI_XFER_LITTLE_ENDIAN) {
 			LOG_INST_ERR(cfg->log, "%u, only support MSB first.", __LINE__);
-			ret = -ENOTSUP;
-			goto e_return;
-		}
-
-		if (dev_cfg->bDQSEnable && !cfg->mspicfg.bDQS) {
-			LOG_INST_ERR(cfg->log, "%u, only support non-DQS mode.", __LINE__);
 			ret = -ENOTSUP;
 			goto e_return;
 		}
@@ -764,16 +723,16 @@ static int mspi_ambiq_dev_config(const struct device *controller,
 			goto e_return;
 		}
 
-		am_hal_mspi_dqs_t dqsCfg = g_sMspiDqsCfg;
-		ret = am_hal_mspi_control(data->mspiHandle, AM_HAL_MSPI_REQ_DQS, &dqsCfg);
+		am_hal_mspi_dqs_t * dqsCfg = &g_sMspiDqsCfg;
+		dqsCfg->bDQSEnable = dev_cfg->bDQSEnable;
+		ret = am_hal_mspi_control(data->mspiHandle, AM_HAL_MSPI_REQ_DQS, dqsCfg);
 		if (ret) {
 			LOG_INST_ERR(cfg->log, "%u, failed AM_HAL_MSPI_REQ_DQS, code:%d.", __LINE__, ret);
 			ret = -EHOSTDOWN;
 			goto e_return;
 		}
 
-		am_hal_mspi_rxcfg_t RxCfg = g_sMspiRxCfg;
-		ret = am_hal_mspi_control(data->mspiHandle, AM_HAL_MSPI_REQ_RXCFG, &RxCfg);
+		ret = am_hal_mspi_control(data->mspiHandle, AM_HAL_MSPI_REQ_RXCFG, &data->hal_rx_cfg);
 		if (ret) {
 			LOG_INST_ERR(cfg->log, "%u, failed AM_HAL_MSPI_REQ_RXCFG, code:%d.", __LINE__, ret);
 			ret = -EHOSTDOWN;
@@ -1442,6 +1401,20 @@ static struct mspi_driver_api mspi_ambiq_driver_api = {
 		.eBEPolarity        = AM_HAL_MSPI_BE_LOW_ENABLE,                                \
 	}
 
+#define MSPI_HAL_RX_CFG(n)                                                              \
+	{                                                                                   \
+		.ui8DQSturn         = 2,                                                        \
+		.bRxHI              = 0,                                                        \
+		.bTaForth           = 0,                                                        \
+		.bHyperIO           = 0,                                                        \
+		.ui8RxSmp           = 1,                                                        \
+		.bRBX               = 0,                                                        \
+		.bWBX               = 0,                                                        \
+		.bSCLKRxHalt        = 0,                                                        \
+		.bRxCapEXT          = 0,                                                        \
+		.ui8Sfturn          = 0,                                                        \
+	}
+
 #define AMBIQ_MSPI_DEFINE(n)                                                            \
 	LOG_INSTANCE_REGISTER(DT_DRV_INST(n), mspi##n, CONFIG_MSPI_LOG_LEVEL);              \
 	MSPI_PINCTRL_DT_DEFINE(DT_DRV_INST(n));                                             \
@@ -1461,6 +1434,7 @@ static struct mspi_driver_api mspi_ambiq_driver_api = {
 		.hal_dev_cfg           = MSPI_HAL_DEVICE_CONFIG(n),                             \
 		.hal_xip_cfg           = MSPI_HAL_XIP_CONFIG(n),                                \
 		.hal_xip_misc_cfg      = MSPI_HAL_XIP_MISC_CONFIG(n),                           \
+		.hal_rx_cfg            = MSPI_HAL_RX_CFG(n),                                    \
 		.dev_id                = 0,                                                     \
 		.lock                  = Z_MUTEX_INITIALIZER(mspi_ambiq_data##n.lock),          \
 		.dev_cfg               = {0},                                                   \
