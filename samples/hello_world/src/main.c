98add73803db78d1adc040069f29f21ae75117a9
Aaron Ye <aye@ambiq.com>
2024-06-14 15:04:05 +0800

    samples: hello_world: update log prints
    Wse printk and LOG module for SWO demonstration
    
    Signed-off-by: Aaron Ye <aye@ambiq.com>

--- samples/hello_world/src/main.c
+++ samples/hello_world/src/main.c
@@ -5,10 +5,22 @@
  */
 
 #include <stdio.h>
+#include <zephyr/sys/printk.h>
+
+#define LOG_LEVEL LOG_LEVEL_DBG
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(app);
 
 int main(void)
 {
-	printf("Hello World! %s\n", CONFIG_BOARD_TARGET);
+	printk("Hello World! %s\n", CONFIG_BOARD_TARGET);
+
+	LOG_ERR("This is a error log");
+	LOG_WRN("This is a warning log");
+	LOG_INF("This is a information log");
+	LOG_DBG("This is a debug log");
+
+	printk("Done with prints\n");
 
 	return 0;
 }
