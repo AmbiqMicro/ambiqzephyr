f6a91cf891501d7e24aac3f56b3bfbbd09e01fdd
BH Li <bli@ambiq.com>
2025-09-11 10:46:10 +0800

    boards: ambiq: update board runner args for build system for apollo510L_eb.
    Protect mcuboot from erasing when sysbuild flashing using west flash.
    
    Signed-off-by: BH Li <bli@ambiq.com>

--- boards/ambiq/apollo510L_eb/board.cmake
+++ boards/ambiq/apollo510L_eb/board.cmake
@@ -1,6 +1,11 @@
 # Copyright (c) 2025 Ambiq Micro Inc.
 # SPDX-License-Identifier: Apache-2.0
 
-board_runner_args(jlink "--device=AP510L" "--iface=swd" "--speed=4000" "--erase" "--reset")
+if(CONFIG_BOOTLOADER_MCUBOOT OR CONFIG_MCUBOOT)
+board_runner_args(jlink "--device=AP510L" "--iface=swd" "--speed=4000")
+else()
+board_runner_args(jlink "--device=AP510L" "--iface=swd" "--speed=4000" "--erase")
+endif()
+
 
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
