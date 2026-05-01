0429cce3c6db4e52fef40a11bc524dccc2670a95
Richard Wheatley <richard.wheatley@ambiq.com>
2026-05-01 14:34:03 -0500

    drivers: input: add support for Chipsemi CHSC6540/6x40 touch controller
    Add a new input driver for the Chipsemi CHSC6540/6x40 self-capacitive
    touch panel controller. This driver implements the correct I²C protocol
    and data format for the CHSC6540/6x40 series, supporting up to two
    touch points and proper interrupt/reset handling.
    
    This enables reliable touch operation on platforms using the
    CHSC6540/6x40 controller and provides a foundation for future
    enhancements and board integrations.
    
    Signed-off-by: Richard Wheatley <richard.wheatley@ambiq.com>

---
+++ drivers/input/input_chsc6540.c
@@ -0,0 +1,146 @@
+/*
+ * Copyright (c) 2026 Your Name
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#define DT_DRV_COMPAT chipsemi_chsc6540
+
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/i2c.h>
+#include <zephyr/input/input.h>
+#include <zephyr/input/input_touch.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/pm/device.h>
+
+LOG_MODULE_REGISTER(chsc6540, CONFIG_INPUT_LOG_LEVEL);
+
+struct chsc6540_config {
+	struct input_touchscreen_common_config common;
+	struct i2c_dt_spec i2c;
+	const struct gpio_dt_spec int_gpio;
+	const struct gpio_dt_spec reset_gpio;
+};
+
+struct chsc6540_data {
+	const struct device *dev;
+	struct k_work work;
+	struct gpio_callback int_gpio_cb;
+};
+
+#define CHSC6540_I2C_ADDR 0x2E
+#define CHSC6540_TOUCH_DATA_REG 0x00
+#define CHSC6540_TOUCH_DATA_LEN 16
+
+static void chsc6540_work_handler(struct k_work *work)
+{
+	struct chsc6540_data *data = CONTAINER_OF(work, struct chsc6540_data, work);
+	const struct device *dev = data->dev;
+	const struct chsc6540_config *cfg = dev->config;
+	uint8_t touch_data[CHSC6540_TOUCH_DATA_LEN] = {0};
+	uint8_t reg = CHSC6540_TOUCH_DATA_REG;
+	int ret = i2c_write_read_dt(&cfg->i2c, &reg, 1, touch_data, CHSC6540_TOUCH_DATA_LEN);
+	if (ret < 0) {
+		LOG_ERR("CHSC6540: Could not read touch data: %d", ret);
+		return;
+	}
+	uint8_t num_points = touch_data[2] & 0x0F;
+	bool is_pressed = num_points > 0;
+	if (num_points >= 1) {
+		uint16_t x1 = ((uint16_t)touch_data[3] << 8) | touch_data[4];
+		uint16_t y1 = ((uint16_t)touch_data[5] << 8) | touch_data[6];
+		input_touchscreen_report_pos(dev, x1, y1, K_FOREVER);
+	}
+	if (num_points >= 2) {
+		uint16_t x2 = ((uint16_t)touch_data[9] << 8) | touch_data[10];
+		uint16_t y2 = ((uint16_t)touch_data[11] << 8) | touch_data[12];
+		// Optionally: report second touch if supported
+	}
+	input_report_key(dev, INPUT_BTN_TOUCH, is_pressed, true, K_FOREVER);
+}
+
+static void chsc6540_isr_handler(const struct device *dev, struct gpio_callback *cb, uint32_t mask)
+{
+	struct chsc6540_data *data = CONTAINER_OF(cb, struct chsc6540_data, int_gpio_cb);
+	k_work_submit(&data->work);
+}
+
+static int chsc6540_reset(const struct device *dev)
+{
+	const struct chsc6540_config *config = dev->config;
+	int ret;
+	if (config->reset_gpio.port != NULL) {
+		if (!gpio_is_ready_dt(&config->reset_gpio)) {
+			LOG_ERR("GPIO port %s not ready", config->reset_gpio.port->name);
+			return -ENODEV;
+		}
+		ret = gpio_pin_configure_dt(&config->reset_gpio, GPIO_OUTPUT_ACTIVE);
+		if (ret < 0) {
+			LOG_ERR("Could not configure reset GPIO (%d)", ret);
+			return ret;
+		}
+		k_usleep(500);
+		ret = gpio_pin_set_dt(&config->reset_gpio, 0);
+		if (ret < 0) {
+			LOG_ERR("Could not pull reset low (%d)", ret);
+			return ret;
+		}
+		k_msleep(1);
+		ret = gpio_pin_set_dt(&config->reset_gpio, 1);
+		if (ret < 0) {
+			LOG_ERR("Could not pull reset high (%d)", ret);
+			return ret;
+		}
+		k_msleep(50);
+	}
+	return 0;
+}
+
+static int chsc6540_init(const struct device *dev)
+{
+	const struct chsc6540_config *config = dev->config;
+	struct chsc6540_data *data = dev->data;
+	int ret;
+	data->dev = dev;
+	k_work_init(&data->work, chsc6540_work_handler);
+	ret = chsc6540_reset(dev);
+	if (ret < 0) {
+		LOG_ERR("Failed to reset (%d)", ret);
+		return ret;
+	}
+	if (!gpio_is_ready_dt(&config->int_gpio)) {
+		LOG_ERR("GPIO port %s not ready", config->int_gpio.port->name);
+		return -ENODEV;
+	}
+	ret = gpio_pin_configure_dt(&config->int_gpio, GPIO_INPUT);
+	if (ret < 0) {
+		LOG_ERR("Could not configure interrupt GPIO pin: %d", ret);
+		return ret;
+	}
+	ret = gpio_pin_interrupt_configure_dt(&config->int_gpio, GPIO_INT_EDGE_TO_ACTIVE);
+	if (ret < 0) {
+		LOG_ERR("Could not configure interrupt GPIO interrupt: %d", ret);
+		return ret;
+	}
+	gpio_init_callback(&data->int_gpio_cb, chsc6540_isr_handler, BIT(config->int_gpio.pin));
+	ret = gpio_add_callback(config->int_gpio.port, &data->int_gpio_cb);
+	if (ret < 0) {
+		LOG_ERR("Could not set gpio callback: %d", ret);
+		return ret;
+	}
+	return 0;
+}
+
+#define CHSC6540_DEFINE(index) \
+	static const struct chsc6540_config chsc6540_config_##index = { \
+		.common = INPUT_TOUCH_DT_INST_COMMON_CONFIG_INIT(index), \
+		.i2c = I2C_DT_SPEC_INST_GET(index), \
+		.int_gpio = GPIO_DT_SPEC_INST_GET(index, int_gpios), \
+		.reset_gpio = GPIO_DT_SPEC_INST_GET_OR(index, reset_gpios, {0}), \
+	}; \
+	static struct chsc6540_data chsc6540_data_##index; \
+	DEVICE_DT_INST_DEFINE(index, chsc6540_init, NULL, \
+			      &chsc6540_data_##index, &chsc6540_config_##index, POST_KERNEL, \
+			      CONFIG_INPUT_INIT_PRIORITY, NULL);
+
+DT_INST_FOREACH_STATUS_OKAY(CHSC6540_DEFINE)
