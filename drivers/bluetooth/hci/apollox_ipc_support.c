4c08f8eaf9f53ddf05402d0c36b77f0841707dc4
Eddy Hartono <eddy.hartono@ambiq.com>
2026-05-19 20:14:06 +0800

    Bluetooth: hci: ambiq: add Apollo 330P/510L IPC setup sequence
    Add Apollo 330P/510L vendor setup support for the IPC HCI transport.
    
    The setup sequence programs the 330P/510L NVDS parameters, updates the
    controller LE feature mask, configures TX power, and sets the BD address
    before normal HCI initialization.
    
    Enables controller features needed for pairing, bonding, privacy,
    and accept-list use cases.
    
    Move the Apollo 330P/510L feature and TX power definitions into a local
    support header for reuse by the IPC setup code.
    
    Signed-off-by: Eddy Hartono <eddy.hartono@ambiq.com>

--- drivers/bluetooth/hci/apollox_ipc_support.c
+++ drivers/bluetooth/hci/apollox_ipc_support.c
@@ -11,6 +11,7 @@
 #endif
 #include <zephyr/sys/byteorder.h>
 #include <zephyr/drivers/bluetooth.h>
+#include <zephyr/bluetooth/addr.h>
 #include <zephyr/bluetooth/hci.h>
 #include <zephyr/bluetooth/hci_types.h>
 #include <zephyr/net_buf.h>
@@ -20,34 +21,42 @@
 #include <zephyr/logging/log.h>
 
 #include <am_rss_mgr.h>
+#include <am_mcu_apollo.h>
+
+#include "apollox_ipc_support.h"
 
 LOG_MODULE_REGISTER(bt_hci_apollox_ipc_support);
 
 #if defined(CONFIG_SOC_APOLLO510L) || defined(CONFIG_SOC_APOLLO330P)
 /* CEVA/RSS 510L vendor opcodes (AmbiqSuite hci_vs_510L_radio / hci_drv_510L_radio) */
 #define AMBIQ_HCI_OP_VS_510L_UPDATE_NVDS       BT_OP(BT_OGF_VS, 0x0080)
+#define AMBIQ_HCI_OP_VS_510L_SET_BD_ADDR       BT_OP(BT_OGF_VS, 0x0081)
 #define AMBIQ_HCI_OP_VS_510L_UPDATE_LL_FEATURE BT_OP(BT_OGF_VS, 0x0082)
 #define AMBIQ_HCI_OP_VS_510L_SET_TX_POWER      BT_OP(BT_OGF_VS, 0x0083)
+#define AMBIQ_HCI_OP_VS_510L_GET_CON_EVT_CNT   BT_OP(BT_OGF_VS, 0x0085)
+#define AMBIQ_HCI_OP_VS_510L_GET_DTM_RSSI      BT_OP(BT_OGF_VS, 0x0086)
 
-#define AMBIQ_NVDS_CFG_PAYLOAD_LEN    240U
-#define AMBIQ_510L_LPCLK_DRIFT_PPM    500U
-#define AMBIQ_510L_EXT_WAKEUP_TIME_US 800U
-#define AMBIQ_510L_OSC_WAKEUP_TIME_US 800U
-#define AMBIQ_510L_RM_WAKEUP_TIME_US  800U
-#define AMBIQ_510L_POST_NVDS_DELAY_MS 1U
-#define AMBIQ_510L_LL_FEATURE_MASK    0ULL
-#define AMBIQ_510L_VS_TX_POWER_LEVEL  0
+#define AMBIQ_NVDS_CFG_PAYLOAD_LEN          240U
+#define APOLLO_330P_510L_LPCLK_DRIFT_PPM    500U
+#define APOLLO_330P_510L_EXT_WAKEUP_TIME_US 800U
+#define APOLLO_330P_510L_OSC_WAKEUP_TIME_US 800U
+#define APOLLO_330P_510L_RM_WAKEUP_TIME_US  800U
+#define APOLLO_330P_510L_POST_NVDS_DELAY_MS 1U
 
 /* NVDS parameter tags (CEVA/RSS 510L; align with AmbiqSuite NVDS / hci_vs_510L_radio) */
-#define AMBIQ_NVDS_PARAM_ID_LPCLK_DRIFT     0x07
-#define AMBIQ_NVDS_PARAM_ID_EXT_WAKEUP_TIME 0x0d
-#define AMBIQ_NVDS_PARAM_ID_OSC_WAKEUP_TIME 0x0e
-#define AMBIQ_NVDS_PARAM_ID_RM_WAKEUP_TIME  0x0f
-#define AMBIQ_NVDS_PARAM_ID_SLEEP_ENABLE    0x11
-#define AMBIQ_NVDS_PARAM_ID_TRACER_CONFIG   0x2f
-#define AMBIQ_NVDS_PARAM_ID_LE_QOS_ENABLE   0x51
+#define AMBIQ_NVDS_PARAM_ID_BD_ADDRESS       0x01
+#define AMBIQ_NVDS_PARAM_ID_LPCLK_DRIFT      0x07
+#define AMBIQ_NVDS_PARAM_ID_EXT_WAKEUP_TIME  0x0d
+#define AMBIQ_NVDS_PARAM_ID_OSC_WAKEUP_TIME  0x0e
+#define AMBIQ_NVDS_PARAM_ID_RM_WAKEUP_TIME   0x0f
+#define AMBIQ_NVDS_PARAM_ID_SLEEP_ENABLE     0x11
+#define AMBIQ_NVDS_PARAM_ID_TRACER_CONFIG    0x2f
+#define AMBIQ_NVDS_PARAM_ID_MEM_WRITE_ENABLE 0x50
+#define AMBIQ_NVDS_PARAM_ID_LE_QOS_ENABLE    0x51
 
-static void ambiq_510l_nvds_add_u8(uint8_t *p, size_t *o, uint8_t param_id, uint8_t value)
+#define APOLLO_330P_510L_TRACER_CONFIG 0x00080000U
+
+static void apollo_330p_510l_nvds_add_u8(uint8_t *p, size_t *o, uint8_t param_id, uint8_t value)
 {
 	p[(*o)++] = param_id;
 	p[(*o)++] = 0x06;
@@ -55,7 +64,7 @@ static void ambiq_510l_nvds_add_u8(uint8_t *p, size_t *o, uint8_t param_id, uint
 	p[(*o)++] = value;
 }
 
-static void ambiq_510l_nvds_add_le16(uint8_t *p, size_t *o, uint8_t param_id, uint16_t value)
+static void apollo_330p_510l_nvds_add_le16(uint8_t *p, size_t *o, uint8_t param_id, uint16_t value)
 {
 	p[(*o)++] = param_id;
 	p[(*o)++] = 0x06;
@@ -64,7 +73,7 @@ static void ambiq_510l_nvds_add_le16(uint8_t *p, size_t *o, uint8_t param_id, ui
 	*o += sizeof(value);
 }
 
-static void ambiq_510l_nvds_add_le32(uint8_t *p, size_t *o, uint8_t param_id, uint32_t value)
+static void apollo_330p_510l_nvds_add_le32(uint8_t *p, size_t *o, uint8_t param_id, uint32_t value)
 {
 	p[(*o)++] = param_id;
 	p[(*o)++] = 0x06;
@@ -73,7 +82,46 @@ static void ambiq_510l_nvds_add_le32(uint8_t *p, size_t *o, uint8_t param_id, ui
 	*o += sizeof(value);
 }
 
-static void ambiq_510l_build_nvds_sleep_cfg(uint8_t *p)
+static void apollo_330p_510l_nvds_add_addr(uint8_t *p, size_t *o, uint8_t param_id,
+					   const bt_addr_t *addr)
+{
+	p[(*o)++] = param_id;
+	p[(*o)++] = 0x06;
+	p[(*o)++] = sizeof(addr->val);
+	memcpy(&p[*o], addr->val, sizeof(addr->val));
+	*o += sizeof(addr->val);
+}
+
+static int apollo_330p_510l_get_bd_address(const struct bt_hci_setup_params *params,
+					   bt_addr_t *addr)
+{
+	am_hal_mcuctrl_device_t device;
+	uint32_t status;
+
+	if (!bt_addr_eq(&params->public_addr, BT_ADDR_ANY)) {
+		bt_addr_copy(addr, &params->public_addr);
+		return 0;
+	}
+
+	status = am_hal_mcuctrl_info_get(AM_HAL_MCUCTRL_INFO_DEVICEID, &device);
+	if (status != AM_HAL_STATUS_SUCCESS) {
+		LOG_ERR("Failed to read MCU device ID: 0x%08x", status);
+		return -EIO;
+	}
+
+	memcpy(&addr->val[0], &device.ui32ChipID1, sizeof(device.ui32ChipID1));
+	addr->val[4] = (device.ui32ChipID0 >> 8) & 0xff;
+	addr->val[5] = (device.ui32ChipID0 >> 16) & 0xff;
+
+	/*
+	 * Match AmbiqSuite: clear U/L and I/G bits for Android compatibility.
+	 */
+	addr->val[5] &= 0xfc;
+
+	return 0;
+}
+
+static void apollo_330p_510l_build_nvds_cfg(uint8_t *p, const bt_addr_t *addr)
 {
 	size_t o = 0;
 
@@ -83,25 +131,26 @@ static void ambiq_510l_build_nvds_sleep_cfg(uint8_t *p)
 	 * Marks the start of a controller NVDS configuration block for vendor HCI
 	 * update (here: 510L VS 0xFC80; AmbiqSuite: HCI_DBG_UPDATE_NVDS_CFG_CMD_OPCODE).
 	 * Tags that follow carry boot/runtime parameters (e.g. LP drift, wakeup times,
-	 * sleep enable) consumed by the CEVA controller firmware. Full NVDS images in
-	 * other tools can also include items such as BD address; this builder only
-	 * emits the sleep-configuration tags below.
+	 * sleep enable) consumed by the CEVA controller firmware.
 	 */
 	p[o++] = 0x4e;
 	p[o++] = 0x56;
 	p[o++] = 0x44;
 	p[o++] = 0x53;
-	ambiq_510l_nvds_add_le16(p, &o, AMBIQ_NVDS_PARAM_ID_LPCLK_DRIFT,
-				 AMBIQ_510L_LPCLK_DRIFT_PPM);
-	ambiq_510l_nvds_add_le16(p, &o, AMBIQ_NVDS_PARAM_ID_EXT_WAKEUP_TIME,
-				 AMBIQ_510L_EXT_WAKEUP_TIME_US);
-	ambiq_510l_nvds_add_le16(p, &o, AMBIQ_NVDS_PARAM_ID_OSC_WAKEUP_TIME,
-				 AMBIQ_510L_OSC_WAKEUP_TIME_US);
-	ambiq_510l_nvds_add_le16(p, &o, AMBIQ_NVDS_PARAM_ID_RM_WAKEUP_TIME,
-				 AMBIQ_510L_RM_WAKEUP_TIME_US);
-	ambiq_510l_nvds_add_u8(p, &o, AMBIQ_NVDS_PARAM_ID_SLEEP_ENABLE, 1);
-	ambiq_510l_nvds_add_u8(p, &o, AMBIQ_NVDS_PARAM_ID_LE_QOS_ENABLE, 0);
-	ambiq_510l_nvds_add_le32(p, &o, AMBIQ_NVDS_PARAM_ID_TRACER_CONFIG, 0);
+	apollo_330p_510l_nvds_add_addr(p, &o, AMBIQ_NVDS_PARAM_ID_BD_ADDRESS, addr);
+	apollo_330p_510l_nvds_add_u8(p, &o, AMBIQ_NVDS_PARAM_ID_MEM_WRITE_ENABLE, 1);
+	apollo_330p_510l_nvds_add_le16(p, &o, AMBIQ_NVDS_PARAM_ID_LPCLK_DRIFT,
+				       APOLLO_330P_510L_LPCLK_DRIFT_PPM);
+	apollo_330p_510l_nvds_add_le16(p, &o, AMBIQ_NVDS_PARAM_ID_EXT_WAKEUP_TIME,
+				       APOLLO_330P_510L_EXT_WAKEUP_TIME_US);
+	apollo_330p_510l_nvds_add_le16(p, &o, AMBIQ_NVDS_PARAM_ID_OSC_WAKEUP_TIME,
+				       APOLLO_330P_510L_OSC_WAKEUP_TIME_US);
+	apollo_330p_510l_nvds_add_le16(p, &o, AMBIQ_NVDS_PARAM_ID_RM_WAKEUP_TIME,
+				       APOLLO_330P_510L_RM_WAKEUP_TIME_US);
+	apollo_330p_510l_nvds_add_u8(p, &o, AMBIQ_NVDS_PARAM_ID_SLEEP_ENABLE, 1);
+	apollo_330p_510l_nvds_add_u8(p, &o, AMBIQ_NVDS_PARAM_ID_LE_QOS_ENABLE, 0);
+	apollo_330p_510l_nvds_add_le32(p, &o, AMBIQ_NVDS_PARAM_ID_TRACER_CONFIG,
+				       APOLLO_330P_510L_TRACER_CONFIG);
 
 	__ASSERT_NO_MSG(o <= AMBIQ_NVDS_CFG_PAYLOAD_LEN);
 	memset(&p[o], 0, AMBIQ_NVDS_CFG_PAYLOAD_LEN - o);
@@ -130,52 +179,102 @@ static int hci_vs_cmd_send_sync(uint16_t opcode, const void *param, size_t param
 	return err;
 }
 
-static int ambiq_510l_send_nvds_and_pre_reset_vs(const struct device *dev)
+static int apollo_330p_510l_vsc_update_nvds_param(const uint8_t nvds[AMBIQ_NVDS_CFG_PAYLOAD_LEN])
+{
+	return hci_vs_cmd_send_sync(AMBIQ_HCI_OP_VS_510L_UPDATE_NVDS, nvds,
+				    AMBIQ_NVDS_CFG_PAYLOAD_LEN);
+}
+
+static void apollo_330p_510l_build_ll_features(uint8_t ll_features[8])
+{
+	ll_features[0] = (uint8_t)APOLLO_330P_510L_LL_FEATURES_BYTE0;
+	ll_features[1] = (uint8_t)(APOLLO_330P_510L_LL_FEATURES_BYTE1 >> 8);
+	ll_features[2] = (uint8_t)(APOLLO_330P_510L_LL_FEATURES_BYTE2 >> 16);
+	ll_features[3] = (uint8_t)(APOLLO_330P_510L_LL_FEATURES_BYTE3 >> 24);
+	ll_features[4] = (uint8_t)(APOLLO_330P_510L_LL_FEATURES_BYTE4 >> 32);
+	ll_features[5] = (uint8_t)(APOLLO_330P_510L_LL_FEATURES_BYTE5 >> 40);
+	ll_features[6] = (uint8_t)(APOLLO_330P_510L_LL_FEATURES_BYTE6 >> 48);
+	ll_features[7] = (uint8_t)(APOLLO_330P_510L_LL_FEATURES_BYTE7 >> 56);
+}
+
+static int apollo_330p_510l_vsc_update_link_layer_feature(void)
+{
+	uint8_t ll_features[8];
+
+	apollo_330p_510l_build_ll_features(ll_features);
+
+	return hci_vs_cmd_send_sync(AMBIQ_HCI_OP_VS_510L_UPDATE_LL_FEATURE, ll_features,
+				    sizeof(ll_features));
+}
+
+static int
+apollo_330p_510l_vsc_update_tx_power_level(apollo_330p_510l_tx_power_level_t tx_power_level)
+{
+	const int8_t tx_pwr = tx_power_level;
+
+	return hci_vs_cmd_send_sync(AMBIQ_HCI_OP_VS_510L_SET_TX_POWER, &tx_pwr, sizeof(tx_pwr));
+}
+
+int apollo_330p_510l_vsc_set_bd_address(const bt_addr_t *addr)
+{
+	return hci_vs_cmd_send_sync(AMBIQ_HCI_OP_VS_510L_SET_BD_ADDR, addr->val, sizeof(addr->val));
+}
+
+int apollo_330p_510l_vsc_get_conn_event_counter(uint16_t conn_handle)
+{
+	uint8_t param[sizeof(conn_handle)];
+
+	sys_put_le16(conn_handle, param);
+
+	return hci_vs_cmd_send_sync(AMBIQ_HCI_OP_VS_510L_GET_CON_EVT_CNT, param, sizeof(param));
+}
+
+int apollo_330p_510l_vsc_get_dtm_rssi(void)
+{
+	return hci_vs_cmd_send_sync(AMBIQ_HCI_OP_VS_510L_GET_DTM_RSSI, NULL, 0);
+}
+
+static int apollo_330p_510l_send_nvds_and_pre_reset_vs(const struct device *dev,
+						       const struct bt_hci_setup_params *params)
 {
 	uint8_t nvds[AMBIQ_NVDS_CFG_PAYLOAD_LEN];
-	struct net_buf *buf;
-	struct net_buf *rsp = NULL;
+	bt_addr_t bd_addr;
 	int err;
 
 	ARG_UNUSED(dev);
 
-	ambiq_510l_build_nvds_sleep_cfg(nvds);
-
-	buf = bt_hci_cmd_alloc(K_FOREVER);
-	if (!buf) {
-		return -ENOBUFS;
-	}
-
-	net_buf_add_mem(buf, nvds, sizeof(nvds));
-	err = bt_hci_cmd_send_sync(AMBIQ_HCI_OP_VS_510L_UPDATE_NVDS, buf, &rsp);
-	if (rsp != NULL) {
-		net_buf_unref(rsp);
-	}
+	err = apollo_330p_510l_get_bd_address(params, &bd_addr);
 	if (err != 0) {
 		return err;
 	}
-	k_sleep(K_MSEC(AMBIQ_510L_POST_NVDS_DELAY_MS));
+
+	apollo_330p_510l_build_nvds_cfg(nvds, &bd_addr);
+
+	err = apollo_330p_510l_vsc_update_nvds_param(nvds);
+	if (err != 0) {
+		return err;
+	}
+	k_sleep(K_MSEC(APOLLO_330P_510L_POST_NVDS_DELAY_MS));
 
 	/*
 	 * Mirror hci_vs_510L_radio ordering before HCI_Reset. Payloads must match
 	 * AmbiqSuite hci_vs_510L_radio.c if the controller rejects these defaults.
 	 */
-	uint8_t ll_feat[8];
-
-	sys_put_le64(AMBIQ_510L_LL_FEATURE_MASK, ll_feat);
-
-	err = hci_vs_cmd_send_sync(AMBIQ_HCI_OP_VS_510L_UPDATE_LL_FEATURE, ll_feat,
-				   sizeof(ll_feat));
+	err = apollo_330p_510l_vsc_update_link_layer_feature();
 	if (err != 0) {
 		LOG_ERR("510L VS 0xFC82 failed: %d", err);
 		return err;
 	}
 
-	const int8_t tx_pwr = AMBIQ_510L_VS_TX_POWER_LEVEL;
-
-	err = hci_vs_cmd_send_sync(AMBIQ_HCI_OP_VS_510L_SET_TX_POWER, &tx_pwr, sizeof(tx_pwr));
+	err = apollo_330p_510l_vsc_update_tx_power_level(APOLLO_330P_510L_TX_POWER_LEVEL_DEFAULT);
 	if (err != 0) {
 		LOG_ERR("510L VS 0xFC83 failed: %d", err);
+		return err;
+	}
+
+	err = apollo_330p_510l_vsc_set_bd_address(&bd_addr);
+	if (err != 0) {
+		LOG_ERR("510L VS 0xFC81 failed: %d", err);
 		return err;
 	}
 
@@ -242,7 +341,7 @@ int bt_ipc_setup(const struct device *dev, const struct bt_hci_setup_params *par
 	}
 
 #if defined(CONFIG_SOC_APOLLO510L) || defined(CONFIG_SOC_APOLLO330P)
-	ret = ambiq_510l_send_nvds_and_pre_reset_vs(dev);
+	ret = apollo_330p_510l_send_nvds_and_pre_reset_vs(dev, params);
 	if (ret != 0) {
 		LOG_ERR("510L CEVA sleep/VS sequence failed: %d", ret);
 		return ret;
